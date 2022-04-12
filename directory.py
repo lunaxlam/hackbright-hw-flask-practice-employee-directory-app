11a356689883ab607708f176a074136a69c773e5
lunaxlam@gmail.com <lunalam@Lunas-MBP.attlocal.net>
2022-04-12 16:35:34 -0700

    Initial commit

---
+++ directory.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, render_template, redirect, flash
+from model import employee_directory
+
+app = Flask(__name__)
+app.secret_key = '\xf5!\x07!qj\xa4\x08\xc6\xf8\n\x8a\x95m\xe2\x04g\xbb\x98|U\xa2f\x03'
+
+
+@app.route("/")
+def home():
+    """Show the employee directory home page, which has a search form."""
+
+    return render_template("home.html")
+
+
+@app.route("/employee_search")
+def get_employee_details():
+    """Process search and return the employee details page."""
+
+    # Get employee name from search form (passed in the request object)
+    name = request.args.get("employee_name")
+
+    # If the user didn't type anything, ask them to.
+    if not name:
+        flash("Please type in a first name.")
+
+    # If the name isn't in our directory, flash a message to the user.
+    elif name.lower() not in employee_directory:
+        flash(f"{name} not found.")
+
+    # We have a name, and it's in our directory. Return that info.
+    else:
+        employee_info = employee_directory.get(name.lower())
+        return render_template("employee_details.html", details=employee_info)
+
+    # If we didn't return the employee details page, send the user back to the
+    # page they were on before.
+    return(redirect(request.referrer))
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0")
---
+++ model.py
@@ -0,0 +1,92 @@
+employee_directory = {
+    "joel": {
+        "name": "Joel Burton",
+        "email": "joel@hackbrightacademy.com",
+        "favorite_food": "peanut-butter pretzels",
+        "position": "Vice President of Education"
+    },
+
+    "henry": {
+        "name": "Henry Chen",
+        "email": "henry@hackbrightacademy.com",
+        "favorite_food": "Chinese donuts",
+        "position": "Instructor"
+    },
+
+    "meggie": {
+        "name": "Meggie Mahnken",
+        "email": "meggie@hackbrightacademy.com",
+        "favorite_food": "Foods with single, overpowering flavors",
+        "position": "Instructor"
+    },
+
+    "bonnie": {
+        "name": "Bonnie Schulkin",
+        "email": "bonnie@hackbrightacademy.com",
+        "favorite_food": "chocolate cake with raspberries",
+        "position": "Instructor"
+    },
+
+    "kara": {
+        "name": "Kara DeWalt",
+        "email": "kara@hackbrightacademy.com",
+        "favorite_food": "gummy bears",
+        "position": "Lab Instructor"
+    },
+
+    "meg": {
+        "name": "Meg Bishop",
+        "email": "meg@hackbrightacademy.com",
+        "favorite_food": "waffles",
+        "position": "Lab Instructor"
+    },
+
+    "katie": {
+        "name": "Katie Byers",
+        "email": "katiebyers@hackbrightacademy.com",
+        "favorite_food": "sushi",
+        "position": "Lab Instructor"
+    },
+
+    "leslie": {
+        "name": "Leslie Castellanos",
+        "email": "leslie@hackbrightacademy.com",
+        "favorite_food": "dark chocolate",
+        "position": "Teaching Assistant"
+    },
+
+    "allyson": {
+        "name": "Allyson McKnight",
+        "email": "ally@hackbrightacademy.com",
+        "favorite_food": "jamon iberico",
+        "position": "Lab Instructor"
+    },
+
+    "jenn": {
+        "name": "Jennifer Griffith-Delgado",
+        "email": "jennifer@hackbrightacademy.com",
+        "favorite_food": "warm chocolate chip cookies a la mode",
+        "position": "Teaching Assistant"
+    },
+
+    "agnes": {
+        "name": "Agnes Klimaite",
+        "email": "agnes@hackbrightacademy.com",
+        "favorite_food": "havarti cheese",
+        "position": "Teaching Assistant"
+    },
+
+    "balloonicorn": {
+        "name": "Balloonicorn",
+        "email": "balloonicorn@hackbrightacademy.com",
+        "favorite_food": "magical rainbow cupcakes with iridescent sprinkles",
+        "position": "Support Staff"
+    },
+
+    "mel": {
+        "name": "Mel",
+        "email": "mel@ubermelon.com",
+        "favorite_food": "Powerbars",
+        "position": "The Boss"
+    }
+}
